4034b243086a0f36013ebace08ca92deaadca8b8
isack2 <isack2@naver.com>
2021-01-03 16:17:59 +0900

    chapter1

--- 3-1.c
+++ 3-1.c
@@ -1,52 +1,34 @@
 #include<stdio.h>
-
-int multiple(int);
-int unmultiple(int);
+#include<limits.h>
+#include<float.h>
 
 int main(void){
-    int size_char = sizeof(char);
-    int size_unchar = sizeof(unsigned char);
-    int size_int = sizeof(int);
-    int size_unint = sizeof(unsigned int);
-    int size_float = sizeof(float);
 
-    char val_char = multiple(size_char);
-    unsigned char val_unchar = unmultiple(size_unchar);
-    int val_int = multiple(size_int);
-    unsigned int val_unint = unmultiple(size_unint);
-    float val_float = multiple(size_float);
+    char val_charmin = CHAR_MIN;
+    char val_charmax = CHAR_MAX;
+    char val_charover = CHAR_MAX + 1; //over flow
+    unsigned char val_ucharmin = UCHAR_MAX + 1; //over flow
+    unsigned char val_ucharmax = UCHAR_MAX;
+    int val_intmin = INT_MIN;
+    int val_intmax = INT_MAX;
+    int val_intover = INT_MAX + 1;
+    unsigned int val_uintmin = UINT_MAX + 1; //over flow
+    unsigned int val_uintmax = UINT_MAX;
+    float val_floatmin = FLT_MIN;
+    float val_floatmax = FLT_MAX;
+    float val_floatover = FLT_MAX + FLT_MAX;
 
-    printf("size of char : %d\n", size_char);
-    printf("size of unchar : %d\n", size_unchar);
-    printf("size of int : %d\n", size_int);
-    printf("size of unint : %d\n", size_unint);
-
-    printf("range of char : %d ~ %d\n", -val_char, val_char);
-    printf("range of unchar : %u ~ %u\n", 0, val_unchar);
-    printf("range of int : %d ~ %d\n", -val_int, val_int);
-    printf("range of unint : %u ~ %u\n", 0,val_unint);
-
-    printf("overflow char : %d\n", val_char+1);
-    printf("overflow unchar : %d\n", val_unchar+1);
-    printf("overflow int : %d\n", val_int+1);
-    printf("overflow unint : %d\n", val_unint+1);
- 
-}
-
-int multiple(int size){
-    int value = 2;
-    for(int i = 1 ; i < (8 * size - 1); i++){
-        value = (value * 2);
-    }
-
-    return value - 1;
-}
-
-int unmultiple(int size){
-    int value = 2;
-    for(int i = 1 ; i < (8 * size) ; i++){
-        value = (value * 2);
-    }
-
-    return value - 1;
+    printf("char min : %d\n", val_charmin);
+    printf("char max : %d\n", val_charmax);
+    printf("char overflow : %d\n", val_charover); //over flow
+    printf("unsigned char min : %u\n", val_ucharmin);
+    printf("unsigned char max : %u\n", val_ucharmax);
+    printf("int min : %d\n", val_intmin);
+    printf("int max : %d\n", val_intmax);
+    printf("int overflow : %d\n", val_intover); //over flow
+    printf("unsigned int min : %u\n", val_uintmin);
+    printf("unsigned int max : %u\n", val_uintmax);
+    printf("float min : %.60f\n", val_floatmin);
+    printf("float max : %.60f\n", val_floatmax);
+    printf("float overflow : %.60f\n", val_floatover);
 }
