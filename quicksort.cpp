56b4d3eae46ede242f9a994ba7f0407dcf50cdbb
isack2 <isack2@naver.com>
2021-03-22 23:40:56 +0900

    quicksort

--- quicksort.cpp
+++ quicksort.cpp
@@ -2,7 +2,6 @@
 
 void quicksort(int *, int);
 void swap(int *, int *);
-int count = 0;
 
 int main(void){
     using namespace std;
@@ -44,48 +43,25 @@ void quicksort(int *arr, int size){
     right = size - 1;
     left = pivot + 1;
 
-    while(left <= right){
-        while((left < right) && (left < size - 1)){
-            while((arr[left] < arr[pivot]) && (left < size - 1)){
-                left++;
-                }
-            while((arr[right] > arr[pivot]) && (right > 0)){
-                right--;
-                }
-            if((left >= right) && (arr[0] > arr[right])){
-                swap(&arr[0], &arr[right]);
-                break;
-                }
-            else{
-                swap(&arr[left], &arr[right]);
-                }
+    if(size <= 1){
+        return;
+    }
+    while(size > 1){
+        while((arr[left] < arr[pivot]) && (left < size - 1)){
+            left++;
             }
-        }
-        quicksort(arr, right);
-        quicksort(arr + right + 1, size - right - 1);
-        }
-
-// 1 2 9 6 4 (left = 1, arr[left] = 2 , right = 0, arr[right] = 1, arr[pivot] = 1)
-// 1 2 9 6 4
-// 1 2 9 6 4
-// 1 2 4 6 9
-
-// 5 7 3 9 2
-// 5 2 3 9 7
-// 3 2 5 9 7 (1st)
-// 2 3 5 9 7 
-// 2 3 5 7 9
-
-
-// 9 1 3 6 7 (left = 4, right = 4, arr[left] = 7, arr[right] = 7, arr[pivot] = 9)
-// 7 1 3 6 9 
-// 6 1 3 7 9
-// 3 1 6 7 9
-// 1 3 6 7 9
-
-
-
-// 2 9 1 6 4
-// 2 1 9 6 4 (left = 9 , right = 1) 
-// 1 2 9 6 4 
-
+        while((arr[right] > arr[pivot]) && (right > pivot)){
+            right--;
+            }
+        if((left >= right) && (arr[pivot] >= arr[right])){
+            swap(&arr[pivot], &arr[right]);
+            pivot = right;
+            break;
+            }
+        else{
+            swap(&arr[left], &arr[right]);
+            }
+    }   
+    quicksort(arr, pivot);
+    quicksort(&arr[pivot] + 1, size - pivot - 1);
+}
